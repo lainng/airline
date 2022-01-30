b107c4a4b31b825d89f5fa98349b62f638a3baf0
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-01-30 20:40:24 +0300

    Updated javadoc

--- src/main/java/com/epamtc/airline/dao/CrewDao.java
+++ src/main/java/com/epamtc/airline/dao/CrewDao.java
@@ -1,8 +1,6 @@
 package com.epamtc.airline.dao;
 
 import com.epamtc.airline.dao.exception.DaoException;
-import com.epamtc.airline.entity.Flight;
-import com.epamtc.airline.entity.User;
 import com.epamtc.airline.entity.dto.CrewCreationDto;
 import com.epamtc.airline.entity.dto.CrewDto;
 
@@ -11,15 +9,46 @@ import java.util.List;
 public interface CrewDao {
 
     /**
-     * Fetches {@link List} of {@link User} entities that entries to crew.
-     * @param flightID the ID of {@link Flight} instance.
-     * @return {@link List} of {@link User} entities that entries to crew, or empty {@link List} if the crew not found.
+     * Fetches a crew by the flight ID.
+     * @param flightID The ID of the flight.
+     * @return An {@link CrewDto} instance or {@code null} if there are no crew by specified flight ID.
      * @throws DaoException if a data source access error or other errors.
      */
     CrewDto findCrewByFlightID(long flightID) throws DaoException;
+
+    /**
+     * Fetches a crew by its ID.
+     * @param crewID The ID of the crew.
+     * @return An {@link CrewDto} instance or {@code null} if there are no crew by specified ID.
+     * @throws DaoException if a data source access error or other errors.
+     */
     CrewDto findCrewByID(long crewID) throws DaoException;
+
+    /**
+     * Fetches a list of all crews.
+     * @return The {@link List} of {@link CrewDto} entities, or an empty {@link List} if there are no crews.
+     * @throws DaoException if a data source access error or other errors.
+     */
     List<CrewDto> findAllCrews() throws DaoException;
+
+    /**
+     * Updates the crew information.
+     * @param crewCreationDto An {@link CrewCreationDto} instance that contains updated information.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void updateCrew(CrewCreationDto crewCreationDto) throws DaoException;
+
+    /**
+     * Adds crew to the data source.
+     * @param crewCreationDto An {@link CrewCreationDto} instance.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void addCrew(CrewCreationDto crewCreationDto) throws DaoException;
+
+    /**
+     * Deleted the crew.
+     * @param crewID The ID of the crew to be removed.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void deleteCrew(long crewID) throws DaoException;
 }
