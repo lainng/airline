e2cc380144c3fa09daeca38e74365bcc64ab2b72
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-01-30 21:35:57 +0300

    Updated serialVersionUID field

--- src/main/java/com/epamtc/airline/service/exception/ServiceException.java
+++ src/main/java/com/epamtc/airline/service/exception/ServiceException.java
@@ -1,6 +1,8 @@
 package com.epamtc.airline.service.exception;
 
 public class ServiceException extends Exception {
+    private static final long serialVersionUID = 1L;
+
     public ServiceException() {
         super();
     }
