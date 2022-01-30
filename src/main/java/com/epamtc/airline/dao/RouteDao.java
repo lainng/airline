173f42f32b4a72ed7f762b917b5497384374fcf2
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-01-30 19:04:46 +0300

    Updated javadoc

--- src/main/java/com/epamtc/airline/dao/RouteDao.java
+++ src/main/java/com/epamtc/airline/dao/RouteDao.java
@@ -6,8 +6,33 @@ import com.epamtc.airline.entity.dto.RouteDto;
 import java.util.List;
 
 public interface RouteDao {
+
+    /**
+     * Fetches a route DTO by its ID.
+     * @param routeID Route ID in the data source.
+     * @return An {@link RouteDto} instance that contains raw information of route.
+     * @throws DaoException if a data source access error or other errors.
+     */
     RouteDto findRouteByID(long routeID) throws DaoException;
+
+    /**
+     * Fetches all routes DTOs
+     * @return The {@link List} of the {@link RouteDto} that contains in the data source.
+     * @throws DaoException if a data source access error or other errors.
+     */
     List<RouteDto> findAllRoutes() throws DaoException;
+
+    /**
+     * Added the route to the data source.
+     * @param routeDto An {@link RouteDto} instance that contains raw information of route.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void addRoute(RouteDto routeDto) throws DaoException;
+
+    /**
+     * Updated route information.
+     * @param routeDto An {@link RouteDto} instance that contains raw information of route.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void updateRoute(RouteDto routeDto) throws DaoException;
 }
