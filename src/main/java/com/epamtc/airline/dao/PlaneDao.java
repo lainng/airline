f0188ad254d008c21c38176a39c5f8d97a693f7f
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-01-30 19:12:13 +0300

    Updated javadoc

--- src/main/java/com/epamtc/airline/dao/PlaneDao.java
+++ src/main/java/com/epamtc/airline/dao/PlaneDao.java
@@ -6,8 +6,33 @@ import com.epamtc.airline.entity.Plane;
 import java.util.List;
 
 public interface PlaneDao {
+
+    /**
+     * Fetches the plane by its ID.
+     * @param planeID The plane ID in the data source.
+     * @return An {@link Plane} instance that linked with specified ID.
+     * @throws DaoException if a data source access error or other errors.
+     */
     Plane findPlaneByID(long planeID) throws DaoException;
+
+    /**
+     * Fetches the list of all planes.
+     * @return The {@link List} of the {@link Plane} that contains in the data source.
+     * @throws DaoException if a data source access error or other errors.
+     */
     List<Plane> findAllPlanes() throws DaoException;
+
+    /**
+     * Adds plane to the data source.
+     * @param plane An {@link Plane} entity.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void addPlane(Plane plane) throws DaoException;
+
+    /**
+     * Updates plane information in the data source.
+     * @param plane An {@link Plane} entity that contains changing information.
+     * @throws DaoException if a data source access error or other errors.
+     */
     void updatePlane(Plane plane) throws DaoException;
 }
