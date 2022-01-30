20de5ba7359fd36cd8bae08adb3a2dac3ae39cea
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-01-30 19:13:18 +0300

    Updated javadoc

--- src/main/java/com/epamtc/airline/dao/FlightStatusDao.java
+++ src/main/java/com/epamtc/airline/dao/FlightStatusDao.java
@@ -6,10 +6,10 @@ import com.epamtc.airline.entity.FlightStatus;
 public interface FlightStatusDao {
 
     /**
-     *
-     * @param statusID
-     * @return
-     * @throws DaoException
+     * Fetches flight status by its ID.
+     * @param statusID The ID of status in data source.
+     * @return An {@link FlightStatus} instance.
+     * @throws DaoException if a data source access error or other errors.
      */
     FlightStatus findFlightStatusByID(long statusID) throws DaoException;
 }
