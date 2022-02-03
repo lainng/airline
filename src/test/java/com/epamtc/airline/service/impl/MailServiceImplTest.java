d73c96efce919acbb819c1c5391b0de96f450bf8
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-02-04 00:08:16 +0300

    Added mail service tests

---
+++ src/test/java/com/epamtc/airline/service/impl/MailServiceImplTest.java
@@ -0,0 +1,69 @@
+package com.epamtc.airline.service.impl;
+
+import com.epamtc.airline.dao.connection.ConnectionPool;
+import com.epamtc.airline.entity.Crew;
+import com.epamtc.airline.entity.dto.CrewCreationDto;
+import com.epamtc.airline.service.CrewService;
+import com.epamtc.airline.service.MailService;
+import com.epamtc.airline.service.ServiceFactory;
+import com.epamtc.airline.service.exception.ServiceException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+public class MailServiceImplTest {
+    private final MailService mailService = new MailServiceImpl();
+
+    @BeforeAll
+    static void setUp() {
+        ConnectionPool.getInstance().init();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        ConnectionPool.getInstance().terminate();
+    }
+
+    @Test
+    void sendNewFlightMailTest() throws ServiceException {
+        mailService.sendNewFlightMail(24L, "ru");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void sendNewCrewMailTest() throws ServiceException {
+        mailService.sendNewCrewMail(24L, "ru");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void sendDeleteCrewMailTest() throws ServiceException {
+        CrewService crewService = ServiceFactory.getInstance().getCrewService();
+        Optional<Crew> testCrew = crewService.takeCrewByID(37L);
+        if (!testCrew.isPresent()) {
+            Assertions.fail();
+        }
+        mailService.sendDeleteCrewMail(testCrew.get(),"ru");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void sendEditCrewMailTest() throws ServiceException {
+        CrewCreationDto dtoTest = new CrewCreationDto();
+        dtoTest.setID(37L);
+        dtoTest.setAssignedFlightID(25L);
+        dtoTest.setMembers(new long[] {60L, 61L});
+
+        mailService.sendEditCrewMail(dtoTest, "ru");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void sendCancelFlightMailTest() throws ServiceException {
+        mailService.sendCancelFlightMail(24L, "ru");
+        Assertions.assertTrue(true);
+    }
+}
